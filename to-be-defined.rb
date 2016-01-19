c14466e3aa88101cd87548e34acb4abfd7ed65b0
Blake Strickland <Stricklandb02@gmail.com>
2016-01-19 03:40:32 -0500

    Explain possible moves

--- to-be-defined.rb
+++ to-be-defined.rb
@@ -1,8 +1,25 @@
 #This will be a ruby application for black jack.
+#4-8 Decks, dealer stands on soft 17
 #input will be of a coherent player with strict regulations
 # for input.
 #all possibilies will be accounted for in the end, but to
 #start only the first move will be determined.
+
+
+# Hit
+# Stand
+# Double if allowed, otherwise hit
+# Double if allowed, otherwise stand
+# Split
+# Split if double after split is allowed, otherwise Hit
+# Surrender if allowed, otherise hit
+
+
+
+
+
+
+
 
 #get first card from user in explicit format
 first_card = "Ace of Spades"
